8648229d13307c4953b0dc182c68748702eb4bb8
Eric Gu <eric.changning.gu@gmail.com>
2018-07-13 11:19:39 -0400

    added digit sum

--- src/recursion/recursion.py
+++ src/recursion/recursion.py
@@ -1,11 +1,21 @@
 #recursion can apply to both functions and data structures
-
+import sys
 import pprint
-
+sys.setrecursionlimit(1500)
 pp = pprint.PrettyPrinter(width=700) 
 
-def rec_sum(n):
+def number_sum(n):
     if n == 0:
         return 0 
-    return n + rec_sum(n-1)
+    return n + number_sum(n-1)
+
+def digit_sum(n):
+    # Base case
+    if n < 10:
+        return n
+    
+    # Recursion
+    else:
+        return n % 10 + digit_sum(n // 10)
+        
 
--- tests/test_recursion.py
+++ tests/test_recursion.py
@@ -1,10 +1,18 @@
 import pytest
 from src.recursion import recursion
 
-@pytest.mark.parametrize("n, sum", [
+@pytest.mark.parametrize("n, number_sum", [
     (4,10),
     (5,15),
 ])
 
-def test_rec_sum(n, sum):
-    assert recursion.rec_sum(n) == sum
+def test_number_sum(n, number_sum):
+    assert recursion.number_sum(n) == number_sum
+
+@pytest.mark.parametrize("n, digit_sum", [
+    (1234,10),
+    (5234,14),
+])
+
+def test_digit_sum(n, digit_sum):
+    assert recursion.digit_sum(n) == digit_sum
