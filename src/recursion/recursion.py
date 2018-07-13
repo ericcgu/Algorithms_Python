0123e2c8307cea320363e2acb6eda66108a0274d
Eric Gu <eric.changning.gu@gmail.com>
2018-07-13 01:43:31 -0400

    finished sum recursion

--- src/recursion/recursion.py
+++ src/recursion/recursion.py
@@ -1,5 +1,11 @@
 #recursion can apply to both functions and data structures
 
-def addNumber(n):
-    return n + 1
-    
+import pprint
+
+pp = pprint.PrettyPrinter(width=700) 
+
+def rec_sum(n):
+    if n == 0:
+        return 0 
+    return rec_sum(n-1) + n
+
--- tests/test_recursion.py
+++ tests/test_recursion.py
@@ -1,9 +1,11 @@
 import sys
+import pytest
 from src.recursion import recursion
 
+@pytest.mark.parametrize("n, sum", [
+    (4,10),
+    (5,15),
+])
 
-
-print(sys.path)
-def test_addNumber():
-
-    assert True
+def test_rec_sum(n, sum):
+    assert recursion.rec_sum(n) == sum
