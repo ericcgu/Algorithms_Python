8b66252864a27f38d7771fd1653303435584f8a7
Eric Gu <eric.changning.gu@gmail.com>
2018-07-12 22:01:03 -0400

    add pytest

---
+++ src/recursion/recursion.py
@@ -0,0 +1,5 @@
+#recursion can apply to both functions and data structures
+
+def addNumber(n):
+    return n + 1
+    
---
+++ tests/test_recursion.py
@@ -0,0 +1,9 @@
+import sys
+from src.recursion import recursion
+
+
+
+print(sys.path)
+def test_addNumber():
+
+    assert True
